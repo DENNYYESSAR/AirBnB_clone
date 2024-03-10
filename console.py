86efcbc26eaf72241899c292e42b1644d1bf3122
DENNYYESSAR <dennisokeri5@gmail.com>
2024-03-09 21:33:41 -0800

    AirBnB_clone

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 Module for console command interpreter.
 """
 import cmd
+import re
 from models import storage
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
@@ -13,6 +14,23 @@ from models.place import Place
 from models.review import Review
 from shlex import split
 
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 class HBNBCommand(cmd.Cmd):
     """
