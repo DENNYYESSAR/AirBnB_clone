00f1a5ef55bbff36f56dc746d9a9c5164aa92ccd
DENNYYESSAR <dennisokeri5@gmail.com>
2024-03-09 19:11:59 -0800

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -17,18 +17,19 @@ class BaseModel:
             **kwargs (dict): Keyword arguments that represent instance
                             attributes.
         """
+        formt = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[k] = datetime.strptime(v, formt)
                 else:
                     self.__dict__[k] = v
         else:
             models.storage.new(self)
-            
+
     def __str__(self):
         """
         Returns a string representation of the instance, including the class
